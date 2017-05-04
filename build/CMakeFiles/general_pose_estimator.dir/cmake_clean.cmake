4ee81d1843e6fe96c4abc5e79d1438fa0996f455
wywindz <wangy@csu.ac.cn>
2017-05-04 09:41:14 +0800

    initial commit

---
+++ build/CMakeFiles/general_pose_estimator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/general_pose_estimator.dir/src/general_pose_estimation.cpp.o"
+  "general_pose_estimator.pdb"
+  "general_pose_estimator"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/general_pose_estimator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
